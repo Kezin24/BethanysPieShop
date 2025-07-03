18023a278b00bb54dc7fb4ceb5f8ebc6365fd9c2
Abhishek Yadav <138712341+Kezin24@users.noreply.github.com>
2025-07-04 00:23:44 +0530

    Working with Real Data using entity framework core

--- BethanysPieShop/src/Program.cs
+++ BethanysPieShop/src/Program.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository,MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 
 builder.Services.AddControllersWithViews();
 
@@ -24,6 +24,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
-
+DbInitializer.Seed(app);
 
 app.Run();
